169597ca9004efbd1e49c6810d739da007a54032
Nuno Rosa <nuno.rosa@elastichosts.com>
2014-11-11 10:34:57 +0000

    Change pricing.js into a price map

--- js/calculator/pricing.js
+++ js/calculator/pricing.js
@@ -1,34 +1,55 @@
-define(['lib/knockout', './constants'], function(ko, CONSTANTS) {
+/* globals PRICES, CURRENCY */
 
+define(['lib/knockout', 'lib/underscore'], function(ko, _) {
 
-  function Prices() {
-    var self = this,
-        first_country = 'lon-p';
+  var PRICES_UNIT_CONVERSIONS = {
+    cpu_virtual_machine_per_mhz: 24 * 30 / 1000.0,
+    ram_virtual_machine_per_mb: 24 * 30 / 1024.0,
+    cpu_container_per_mhz: 24 * 30 / 1000.0,
+    ram_container_per_mb: 24 * 30 / 1024.0,
+  };
 
-    for (var key in CONSTANTS.UK_PRICE)
-      self[key] = ko.observable();
+  var PRICES_MAP = {
+    cpu_virtual_machine_per_mhz: 'vm_cpu',
+    ram_virtual_machine_per_mb: 'vm_mem',
+    cpu_container_per_mhz: 'container_cpu',
+    ram_container_per_mb: 'container_mem',
 
-    self._country = ko.observable();
+    // Disk/transfer costs.
+    hdd_per_gb: 'disk',
+    ssd_per_gb: 'ssd',
+    bandwidth_per_gb: 'xfer',
 
-    function update_prices(value) {
-      var prices = CONSTANTS.COUNTRIES_PRICES[value];
-      for (var key in CONSTANTS.UK_PRICE) {
-        self[key](prices[key]);
+    // Extra costs.
+    cost_per_static_ip: 'ip',
+    cost_per_vlan: 'vlan',
+    cost_per_firewall: 'firewall',
+    cost_per_desktopcal: 'msft_6wc_00002',
+    cost_per_winserverweb: 'msft_lwa_00135',
+    cost_per_winserverstd: 'msft_p73_04837',
+    cost_per_winserverent: 'msft_p72_04169',
+    cost_per_mssqlserverweb: 'msft_tfa_00009',
+    cost_per_mssqlserverstd: 'msft_228_03159',
+    cost_per_mssqlserver12: 'msft_p73_04837_2012std'
+  };
+
+  function build_prices() {
+    var result = {};
+
+    _.each(PRICES_MAP, function(value, key) {
+      var price = PRICES[value],
+          conversion;
+      if (_.isUndefined(price))
+        result[key] = ko.observable(0.0);
+      else {
+        conversion = PRICES_UNIT_CONVERSIONS[key] || 1.0;
+        result[key] = ko.observable(parseFloat(price) * conversion);
       }
-    }
-
-    self.country = ko.computed({
-      read: function() {
-        return self._country();
-      },
-      write: function(value) {
-        self._country(value);
-        update_prices(value);
-      },
-      owner: self
     });
-    self.country(first_country);
+
+    result['currency'] = ko.observable(CURRENCY);
+    return result;
   }
-  var prices = new Prices();
-  return prices;
+
+  return build_prices();
 });
